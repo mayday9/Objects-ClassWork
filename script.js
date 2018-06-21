a39604558a96791d728e0eb90d28503ee0dc72c7
mayday9 <themaydaygmt@gmail.com>
2018-06-21 20:23:43 +0300

    123

---
+++ script.js
@@ -0,0 +1,122 @@
+/* var taxi = {
+    make: "Webville Motors",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    mileage: 281341
+};
+
+var cadi = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+};
+
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000,
+};
+
+
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+}; */
+
+/*function  precular(car) {
+    if (car.mileage>10000) {
+        return false;
+    } else if(car.year>1960) {
+        return false;
+    }
+    return true;
+}
+
+var worthALook = precular(chevy);
+
+if(worthALook) {
+    console.log("You gotta check out this " + chevy.make + " " + chevy.model);
+    } else {
+    console.log("You should really pass on the " + chevy.make + " " + chevy.model);
+}
+
+function makeCar () {
+    var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
+    var models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
+    var years = [1955, 1957, 1948, 1954, 1961];
+    var colors = ["red", "blue", "tan", "yellow", "white"];
+    var convertible = [true, false];
+
+    var rand1 = Math.floor(Math.random() * makes.length);
+    var rand2 = Math.floor(Math.random() * models.length);
+    var rand3 = Math.floor(Math.random() * years.length);
+    var rand4 = Math.floor(Math.random() * colors.length);
+    var rand5 = Math.floor(Math.random() * 5) + 1;
+    var rand6 = Math.floor(Math.random() * 2);
+
+    var car = {
+        make: makes[rand1],
+        model: models[rand2],
+        year: years[rand3],
+        color: colors[rand4],
+        passengers: rand5,
+        convertible: convertible[rand6],
+        mileage: 0,
+    };
+    return car;
+}
+
+function displayCar(car) {
+    console.log("Your new car is a " + car.year + " " + car.make + " " + car.model);
+}
+
+var carToSell = makeCar();
+displayCar(carToSell);*/
+
+var fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000,
+    started: false,
+
+    start: function () {
+        this.started = true;
+    },
+
+    stop: function () {
+        this.started = false;
+    },
+
+    drive: function (started) {
+        if(this.started) {
+            alert("Zoom zoom!");
+        } else {
+            alert("You need to start the engine first.");
+        }
+
+    }
+};
+fiat.drive();
+fiat.start();
+fiat.drive();
+fiat.stop();
